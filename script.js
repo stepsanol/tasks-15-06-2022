1c4c0b3ec8a7a1acce8f554b0d586a85f731346f
stepsanol <107567473+stepsanol@users.noreply.github.com>
2022-06-21 20:20:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-const array1 = [5, 2, 1, -10, 8];
-const array2 = [5, 2, 1, -9, 3, 7]; 
-
-function union(array1, array2) {
-    const arr = [...array1, ...array2]
-    const result = [];
-    function removeDuplicates(arr) {
-        const obj = {};
-        let j = 0;
-        for(let i = 1; i < arr.length; i++) {
-            const item = arr[i];
-            if(obj[item] !== 1){
-                obj[item] = 1;
-                result[j++] = item;
-            }
-        }
-        return result;
-    }  
-    removeDuplicates(arr);
-    console.log(result);
+let obj = {
+    'Москва': 'Россия',
+    'Киев': 'Украина',
+    'Минск': 'Беларусь',
 }
-console.log(union(array1, array2));
-document.writeln(union(array1, array2)); // [-10,8,5,2,1,-9,3,7]
+const conclusion = document.querySelector('.text');
+
+    for (let key in obj) {
+        conclusion.innerHTML += key + ' это ' + obj[key] + '<br>';
+    }
